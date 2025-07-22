9ad520771824b7ed2a9f473a3ad4afa8621e73bf
BaeDongHyeon <sasd730@gmail.com>
2025-07-22 19:09:24 +0900

    코테 문제 4개

---
+++ src/cote/other/day4/in_20.java
@@ -0,0 +1,16 @@
+package cote.other.day4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 보류
+public class in_20 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Integer number = Integer.parseInt(br.readLine());
+        String[] numbers = br.readLine().split(" ");
+
+
+    }
+}
